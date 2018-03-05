2e840559e620000cb020b5d3a0eef8a9201fa30c
Alexander Valai <valexxjr@gmail.com>
2018-03-05 19:51:26 +0300

    JavaScript source file added

---
+++ UI/scripts.js
@@ -0,0 +1,244 @@
+var photoPosts = [{
+    id: '1',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2018-02-22T23:00:00'),
+    author: 'Иванов Иван',
+    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+}, {
+    id: '2',
+    description: 'Оскар-2018',
+    createdAt: new Date('2018-03-04T11:00:00'),
+    author: 'Александров Александр',
+    photoLink: 'https://github.com/Valexxjr/student-practice/blob/master/UI/img/road.jpg?raw=true'
+}, {
+    id: '3',
+    description: 'Родные места',
+    createdAt: new Date('2018-03-15T10:00:00'),
+    author: 'valexx',
+    photoLink: 'https://github.com/Valexxjr/student-practice/blob/master/UI/img/road.jpg?raw=true'
+}, {
+    id: '4',
+    description: 'Красивое место',
+    createdAt: new Date('2018-02-10T21:00:00'),
+    author: 'Валай Александр',
+    photoLink: 'https://github.com/Valexxjr/student-practice/blob/master/UI/img/road.jpg?raw=true'
+}, {
+    id: '5',
+    description: 'Это праздник какой-то',
+    createdAt: new Date('2018-01-10T15:00:00'),
+    author: 'Алексей',
+    photoLink: 'https://github.com/Valexxjr/student-practice/blob/master/UI/img/road.jpg?raw=true'
+}, {
+    id: '6',
+    description: 'Новый пост',
+    createdAt: new Date('2018-03-05T19:00:00'),
+    author: 'Александров Александр',
+    photoLink: 'https://github.com/Valexxjr/student-practice/blob/master/UI/img/road.jpg?raw=true'
+}, {
+    id: '7',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2018-02-23T22:00:00'),
+    author: 'Петров Петр',
+    photoLink: 'https://github.com/Valexxjr/student-practice/blob/master/UI/img/road.jpg?raw=true'
+}, {
+    id: '8',
+    description: 'Еще пост',
+    createdAt: new Date('2018-02-23T05:00:00'),
+    author: 'Валай Александр',
+    photoLink: 'https://github.com/Valexxjr/student-practice/blob/master/UI/img/road.jpg?raw=true'
+}, {
+    id: '9',
+    description: 'Пост 3',
+    createdAt: new Date('2018-02-28T23:00:00'),
+    author: 'Валай Александр',
+    photoLink: 'https://github.com/Valexxjr/student-practice/blob/master/UI/img/road.jpg?raw=true'
+}, {
+    id: '10',
+    description: 'Пост н',
+    createdAt: new Date('2018-03-01T10:00:00'),
+    author: 'Валай Александр',
+    photoLink: 'https://github.com/Valexxjr/student-practice/blob/master/UI/img/road.jpg?raw=true'
+}, {
+    id: '11',
+    description: 'Пост!!!',
+    createdAt: new Date('2018-01-23T23:00:00'),
+    author: 'Иванов Иван',
+    photoLink: 'https://github.com/Valexxjr/student-practice/blob/master/UI/img/road.jpg?raw=true'
+}, {
+    id: '12',
+    description: 'Женская сборная!!!',
+    createdAt: new Date('2018-02-24T23:00:00'),
+    author: 'Александров Александр',
+    photoLink: 'https://github.com/Valexxjr/student-practice/blob/master/UI/img/road.jpg?raw=true'
+}, {
+    id: '13',
+    description: 'Пост о Беларуси!!!',
+    createdAt: new Date('2018-03-03T19:00:00'),
+    author: 'Сидоров Петр',
+    photoLink: 'https://github.com/Valexxjr/student-practice/blob/master/UI/img/road.jpg?raw=true'
+}, {
+    id: '14',
+    description: 'Я на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2018-02-19T20:00:00'),
+    author: 'Валай Александр',
+    photoLink: 'https://github.com/Valexxjr/student-practice/blob/master/UI/img/road.jpg?raw=true'
+}, {
+    id: '15',
+    description: 'Пост 5!!!',
+    createdAt: new Date('2018-03-03T23:00:00'),
+    author: 'Иванов Иван',
+    photoLink: 'https://github.com/Valexxjr/student-practice/blob/master/UI/img/road.jpg?raw=true'
+}, {
+    id: '16',
+    description: 'Привет!!!',
+    createdAt: new Date('2018-02-22T23:00:00'),
+    author: 'Александров Александр',
+    photoLink: 'https://github.com/Valexxjr/student-practice/blob/master/UI/img/road.jpg?raw=true'
+}, {
+    id: '17',
+    description: 'Еще мой пост',
+    createdAt: new Date('2018-03-01T15:00:00'),
+    author: 'Сидоров Петр',
+    photoLink: 'https://github.com/Valexxjr/student-practice/blob/master/UI/img/road.jpg?raw=true'
+}, {
+    id: '18',
+    description: 'Пост за постом',
+    createdAt: new Date('2018-02-27T06:00:00'),
+    author: 'Валай Александр',
+    photoLink: 'https://github.com/Valexxjr/student-practice/blob/master/UI/img/road.jpg?raw=true'
+}, {
+    id: '19',
+    description: 'Посты!!!',
+    createdAt: new Date('2018-03-05T19:15:00'),
+    author: 'Валай Александр',
+    photoLink: 'https://github.com/Valexxjr/student-practice/blob/master/UI/img/road.jpg?raw=true'
+}, {
+    id: '20',
+    description: 'Пока последний',
+    createdAt: new Date('2018-03-05T19:20:00'),
+    author: 'Валай Александр',
+    photoLink: 'https://github.com/Valexxjr/student-practice/blob/master/UI/img/road.jpg?raw=true'
+}];
+var module = (function () {
+    function getPhotoPosts(skip, top, filterConfig) {
+        skip = skip || 0;
+        top = top || 10;
+        filterConfig = filterConfig || {};
+        var temp = []
+        if(filterConfig.author !== undefined && filterConfig.author.length > 0 ) {
+            for(i = 0; i < photoPosts.length; i++) {
+                if(photoPosts[i].author == filterConfig.author)
+                    temp.push(photoPosts[i]);
+            }
+        }
+        else {
+            temp = photoPosts;
+        }
+        if(filterConfig.createdAt !== undefined) {
+            for(i = 0; i < temp.length; i++) {
+                if(temp[i].createdAt < filterConfig.createdAt)
+                    temp.splice(i, 1);
+            }
+        }
+        function compareDates(photopost1, photopost2) {
+            return photopost1.createdAt - photopost2.createdAt;
+        }
+        temp.sort(compareDates);
+        return temp.slice(skip, skip + top);
+    }
+
+    function getPhotoPost(id) {
+        for (i = 0; i < photoPosts.length; i++) {
+            if (photoPosts[i].id == id)
+                return photoPosts[i];
+        }
+        return null;
+    }
+
+    function validatePhotoPost(photopost) {
+        if(photopost.id === undefined || photopost.description === undefined || photopost.createdAt === undefined 
+            || photopost.author === undefined || photopost.photoLink === undefined)
+            return false;
+        if(photopost.description.length >= 200)
+            return false;
+        if(photopost.author.length == 0)
+            return false;
+        if(photopost.photoLink.length == 0)
+            return false;
+        
+        return true;
+    }
+    function addPhotoPost(photopost) {
+        if( validatePhotoPost(photopost) ) {
+            photoPosts.push(photopost);
+            return true;
+        }
+        return false;
+    }
+    function removePhotoPost(id) {
+        if(id === null || id === undefined)
+            return false;
+        for(i = 0; i < photoPosts.length; i++) {
+            if(photoPosts[i].id == id) {
+                photoPosts.splice(i, 1);
+                return true;
+            }
+        }
+        return false;
+    }
+    function editPhotoPost(id, photopost) {
+        for(i = 0; i < photoPosts.length; i++) {
+            if(photoPosts[i].id == id) {
+                if(photopost.photoLink !== undefined && photopost.photoLink.length > 0) {
+                    photoPosts[i].photoLink = photopost.photoLink;
+                }
+                if(photopost.description !== undefined && photopost.description.length < 200) {
+                    photoPosts[i].description = photopost.description;
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+    return {
+        getPhotoPost,
+        getPhotoPosts,
+        addPhotoPost,
+        editPhotoPost,
+        removePhotoPost
+    }
+}());
+
+console.log(module.getPhotoPost('3'));
+console.log(module.getPhotoPost('43'));
+console.log(photoPosts.length)
+console.log(module.addPhotoPost({
+    id: '22',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2018-02-23T23:00:00'),
+    author: 'Sanya',
+    photoLink: 'a'
+}));
+console.log(photoPosts.length)
+console.log(module.addPhotoPost({
+    id: '22',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2018-02-23T23:00:00'),
+    author: 'Sanya',
+    photoLink: ''
+}));
+console.log(photoPosts.length)
+console.log(module.removePhotoPost('1'))
+console.log(photoPosts.length)
+console.log(module.removePhotoPost('201'))
+console.log(photoPosts.length)
+console.log(module.editPhotoPost('3', {description: 'Modified value'}))
+console.log(photoPosts[1].description)
+console.log(module.editPhotoPost('3', {}))
+console.log(module.editPhotoPost('3', {photoLink: ''}))
+console.log(photoPosts[1].photoLink)
+console.log(module.getPhotoPosts(12, 3))
+console.log(module.getPhotoPosts(0, 20, {author: 'Валай Александр'}))
+console.log(module.getPhotoPosts(0, 20, {author: 'noname'}))
+console.log(module.getPhotoPosts())
+console.log(module.getPhotoPosts({lalala: '11'}))
